ce1430b3c5ee5e05ba49c2a628084b65ef4e91f4
Swetony <swetonyancelmo@gmail.com>
2025-10-19 23:00:57 -0300

    chore: inicializa projeto Spring Boot JobTrack API

---
+++ src/main/java/com/jobtrack/api/JobTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.jobtrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackApiApplication.class, args);
+	}
+
+}
